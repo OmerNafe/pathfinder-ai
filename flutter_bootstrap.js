1571e2b8915d10dfd5ccc8ac82cc923df1dee8fd
OmerNafe <OmerNafe@users.noreply.github.com>
2026-07-27 13:29:37 +0000

    deploy: a19cf6f972ca5c791a068fbe4f40587dce336190

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1813712409" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3211589628" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
